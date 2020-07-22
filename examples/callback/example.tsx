44a8cf8658e03bef3de4cc2e3b43942c39c087b8
ALEXANDER YUSHKOV <ikariott@gmail.com>
2020-07-22 18:44:35 +0300

    Add callback example

---
+++ examples/callback/example.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useGeneratorCallbackState } from '../../src';
+import { timeoutTask, castResult } from 't-tasks';
+
+export const Example = () => {
+  const [state, setState] = React.useState('Blah');
+
+  const [callback, running, cancel] = useGeneratorCallbackState(
+    function*() {
+      castResult<void>(yield timeoutTask(3000));
+
+      setState(state + '-Blah');
+    },
+    [state, setState],
+  );
+
+  const callbackWrapper = React.useCallback(() => {
+    if (!running) {
+      callback();
+    }
+  }, [callback, running]);
+
+  return (
+    <div style={{ fontFamily: 'Arial' }}>
+      Press this button to start operation canceling current one (if any)
+      <div
+        style={{ background: 'gray', padding: 10, margin: 5 }}
+        onClick={callback}
+      >
+        {state} {running && '...'}
+      </div>
+      Press this button to start operation if none is running at the moment
+      <div
+        style={{ background: 'gray', padding: 10, margin: 5 }}
+        onClick={callbackWrapper}
+      >
+        {state} {running && '...'}
+      </div>
+      Press this button to cancel current operation (if any)
+      <div
+        style={{ background: 'gray', padding: 10, margin: 5 }}
+        onClick={cancel}
+      >
+        Cancel
+      </div>
+    </div>
+  );
+};
